67da877a177e448c2e59bb7fe1bfe42b1ae4ccbd
Pablo <guzman.pablo87@gmail.com>
2019-02-03 12:20:53 -0300

    configuracion de redux

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,3 +1,5 @@
+//import { createStore } from 'redux';
+
 const form = document.getElementById('form');
 form.addEventListener('submit', handleSubmit);
 
@@ -8,3 +10,19 @@ function handleSubmit(event) {
 
 	console.warn('Titulo', title);
 };
+/*
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const initialState = [{
+	"title": "Karma police"
+},{
+	"title": "One more time"
+},{
+	"title": "Opale"
+}];
+
+const store = createStore(
+	() => {},
+	initialState,
+	devTools
+);
+*/
