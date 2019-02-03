f875a60b1b407bf4ce2ee0c1a3283b9a2db154b8
Pablo <guzman.pablo87@gmail.com>
2019-02-03 14:22:51 -0300

    actualizacion de estado con subscribe

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -3,17 +3,6 @@ import { createStore } from 'redux';
 const form = document.getElementById('form');
 form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event) {
-	event.preventDefault();
-	const data = new FormData(form);
-	const title = data.get('title');
-
-	store.dispatch({
-		type: 'ADD_SONG',
-		payload: { title }
-	});
-};
-
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const initialState = [{
 	"title": "Karma police"
@@ -39,11 +28,33 @@ const store = createStore(
 	devTools
 );
 
-const container = document.getElementById('playlist');
-const playlist = store.getState();
+function render() {
+	const container = document.getElementById('playlist');
+	const playlist = store.getState();
 
-playlist.forEach((item, i)=> {
-	const template = document.createElement('p');
-	template.textContent = `${i+1}. item.title`;
-	container.appendChild(template);
-});
+	container.innerHTML = '';
+	playlist.forEach((item, i)=> {
+		const template = document.createElement('p');
+		template.textContent = `${i+1}. ${item.title}`;
+		container.appendChild(template);
+	});
+}
+
+function handleSubmit(event) {
+	event.preventDefault();
+	const data = new FormData(form);
+	const title = data.get('title');
+
+	store.dispatch({
+		type: 'ADD_SONG',
+		payload: { title }
+	});
+};
+
+function handleChange() {
+	render();
+}
+
+render();
+
+store.subscribe(handleChange);
